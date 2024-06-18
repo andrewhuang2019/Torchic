22760d2c3b3fc398fb45e9096b0b4b563c6ea71e
Andysword <andrewhuang2019@gmail.com>
2024-06-18 16:12:32 -0500

    Updated pokemon.py

--- src/pokemon.py
+++ src/pokemon.py
@@ -4,9 +4,22 @@ class Pokemon:
         self.name = name
         self.health = health
         self.attack = attack
-        self.sp_attack = attack
+        self.sp_attack = sp_attack
         self.defense = defense
         self.sp_defense = sp_defense
+
+    def get_health(self):
+        return self.health
     
     def get_attack(self):
         return self.attack
+    
+    def get_sp_attack(self):
+        return self.sp_attack
+
+    def get_defense(self):
+        return self.defense
+    
+    def get_sp_defense(self):
+        return self.sp_defense
+    
