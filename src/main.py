d2974b61b4547a9a5d8810d89b31ba743d81ce1d
Andysword <andrewhuang2019@gmail.com>
2024-06-18 15:59:02 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
 import torch
+import torch._dynamo
 import pandas as pd
 import matplotlib.pyplot as plt
-'''def fn(x):
+
+torch._dynamo.config.suppress_errors = True
+
+def fn(x):
    a = torch.cos(x)
    b = torch.sin(a)
    return b
 new_fn = torch.compile(fn, backend="inductor")
 x = torch.randn(10000)
 input_tensor = x
-a = new_fn(input_tensor)'''
+a = new_fn(input_tensor)
 
 pokemon_data_frame = pd.read_csv('Pokemon.csv')
 moves_data_frame = pd.read_csv('df_moves.csv')
@@ -18,5 +22,5 @@ print(pokemon_data_frame)
 print(moves_data_frame)
 print(learned_moves_data_frame)
 
-'''plt.scatter(x,a)
-plt.show()'''
+plt.scatter(x,a)
+plt.show()
