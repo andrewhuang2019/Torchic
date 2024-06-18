23420a9308b173c222c9143bab128b66af77d98f
Keaton Xu <keatx@ku.edu>
2024-06-18 15:45:09 -0500

    Update main.py
    torch test

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import torch
 import pandas as pd
+import matplotlib.pyplot as plt
+def fn(x):
+   a = torch.cos(x)
+   b = torch.sin(a)
+   return b
+new_fn = torch.compile(fn, backend="inductor")
+x = torch.randn(10000)
+input_tensor = x
+a = new_fn(input_tensor)
 
-def main():
-    x = torch.rand(5,3)
-    print(x)
-    frame = pd.DataFrame([1,1])
-    print(frame)
-
-main()
-
+plt.scatter(x,a)
+plt.show()
