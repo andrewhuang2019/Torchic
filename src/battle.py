f249bc4e41deca3eabe03ecfaf2892be59197b72
Andysword <andrewhuang2019@gmail.com>
2024-06-18 16:16:55 -0500

    Create battle.py

---
+++ src/battle.py
@@ -0,0 +1,11 @@
+class Battle:
+    def __init__(self, pokemon1, pokemon2):
+        self.pokemon1 = pokemon1
+        self.pokemon2 = pokemon2
+
+    def use_move(self, move):
+        
+        pass
+
+
+
