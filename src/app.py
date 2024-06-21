e0aaecd7ca800449a52a5eb125b583e318a22764
Andysword <andrewhuang2019@gmail.com>
2024-06-21 16:30:55 -0500

    Updated font to be pixel
    Updated font in the pygame app feature to be pixel

--- src/app.py
+++ src/app.py
@@ -16,10 +16,10 @@ class App:
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         pygame.display.set_caption("Torchic")
-        self.button1 = Button(350, 450, 200, 50, "Click Me", blue, green)
-        self.button2 = Button(575, 450, 200, 50, "Click Me", blue, green)
-        self.button3 = Button(350, 525, 200, 50, "Click Me", blue, green)
-        self.button4 = Button(575, 525, 200, 50, "Click Me", blue, green)
+        self.button1 = Button(350, 450, 200, 50, "FIGHT", blue, green)
+        self.button2 = Button(575, 450, 200, 50, "PKMN", blue, green)
+        self.button3 = Button(350, 525, 200, 50, "ITEM", blue, green)
+        self.button4 = Button(575, 525, 200, 50, "RUN", blue, green)
 
         self._running = True
  
