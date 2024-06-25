19e405d5ad2f73751ed48d9b0a351b9e2b462b3e
Andysword <andrewhuang2019@gmail.com>
2024-06-25 15:57:32 -0500

    Created battle screen
    Created battle screen in python

--- src/app.py
+++ src/app.py
@@ -1,36 +1,48 @@
 import pygame
 from pygame.locals import *
+
 from button import Button
 from battle import Battle
+from screen import Screen
 
 blue = (0, 0, 255)
 green = (0, 255, 0)
 
 class App:
     def __init__(self):
+        pygame.init()
         self._running = True
         self._display_surf = None
         self.size = self.width, self.height = 800, 600
-        self.button1 = None
-        self.in_battle = False
+
+        self.battle = Battle()
+        self.menu_screen = Screen("Menu")
+        self.battle.make_current_screen()
+        #self.button1 = None
+        self.in_battle = True
  
     def on_init(self):
-        pygame.init()
-        self.battle_screen = Battle()
+        self.menu_screen.make_current_screen()
+        #self.battle_screen = Battle()
         self._running = True
  
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
-        if self.battle_screen.button1_is_clicked(event):
-            print("Button clicked!")
+        
+        '''if self.battle_screen.button1_is_clicked(event):
+            print("Button clicked!")'''
     
     def on_loop(self):
-        self.battle_screen.display_screen()
-        pygame.display.flip()
+        #self.battle_screen.display_screen()
+
+        #self.battle_screen.screen_update()
+        pass
 
     def on_render(self):
-        pass
+        if self.in_battle == True:
+            self.battle.display_screen()
+
     def on_cleanup(self):
         pygame.quit()
  
