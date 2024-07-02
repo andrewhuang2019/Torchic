d83379fd15d15ab854474ceab807a4f6356c65dc
Keaton Xu <keatx@ku.edu>
2024-07-02 15:17:18 -0500

    Create move.py

---
+++ src/move.py
@@ -0,0 +1,6 @@
+class Move:
+    def __init__(self, name, movetype, power, dmg_class):
+        self.name = name   
+        self.type = movetype
+        self.power = power
+        self.dmg_class = dmg_class
